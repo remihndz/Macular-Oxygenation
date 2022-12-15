e91593b6ba7d9becf975a3ae192bf1b28291c4a7
Remi Hernandez <remi.hernandez@liverpool.ac.uk>
2022-12-15 12:34:32 +0000

    Fixed convection matrix.

--- Oxygen-Voxel-Method/src/Tissue.py
+++ Oxygen-Voxel-Method/src/Tissue.py
@@ -44,10 +44,12 @@ class Tissue(object):
         Assembles the matrix Gamma of mass transfer flux balance.
     MakeDiffusion()
         Assembles the matrix Md of the O2 diffusion in tissue.
-    MakeConvection()
+    MakeConvection(**kwargs)
         Assembles the matrix Mc of the O2 convection in vessels.
     MakeReaction()
         Assembles the diagonal matrix R of oxygen consumption in tissue.
+    MakeRHS(**kwargs)
+        Assembles the RHS according to boundary conditions in **kwargs.
     """
  
     def __init__(self, **kwargs):
@@ -79,10 +81,13 @@ class Tissue(object):
         else:
             raise ValueError("Provide either a VascularNetwork' or a valid .cco file.")
 
-        self.CellToSegment = self.Vessels.LabelMesh(self.endotheliumThickness).tocsr(copy=False)
+        self.CellToSegment = self.Vessels.LabelMesh(self.endotheliumThickness)
         return
 
     # Various attribute getters
+    @property
+    def nx(self):
+        return self.Vessels.mesh.nCells
     @property
     def dx(self):
         return self.Vessels.mesh.spacing
@@ -123,6 +128,10 @@ class Tissue(object):
     def MakeMassTransfer(self, U : float):
         """Assembles the mass transfer coefficients between vascular nodes and endothelial grid cells.
 
+        The CellToSegment matrix is of the form [-I C], where -I links
+        a vascular node with itself and C encodes the connectivity of
+        the vascular nodes with endothelial cells.
+
         Parameters
         ----------
         U : float
@@ -133,9 +142,11 @@ class Tissue(object):
         # as an estimate of the contact area between
         # intravascular and endothelial cells.
         # The results is scaled by the number of
-        # endothelial cells attached to the vessel.
+        # endothelial cells attached to the vessel,
+        # namely the number of 1s in a row -1 for
+        # the column corresponding to the node itself.
         
-        A = 2*np.pi*vesselData['radius']*vesselData['length']/self.CellToSegment.sum(axis=1)
+        A = 2*np.pi*vesselData['radius']*vesselData['length']/( self.CellToSegment.sum(axis=1) - 1 )
         # del A
         M = sp.diags(A*U/self.endotheliumThickness,
                      offsets=0, format='csr', dtype=np.float32)
@@ -153,26 +164,99 @@ class Tissue(object):
             Oxygen diffusion coefficient in tissue.
         kt : float
             Oxygen consumption rates in tissue.
+
+        TODO: code the possibility for non-uniform grid.
         """
+
         M = sp.dok_array((self.nVol, self.nVol), dtype=np.float32)
         spacing = self.dx
+        v = np.prod(spacing) # Volume of each cells
+        nCells  = self.nx
         dx,dy,dz = spacing
         for cellFlatIndex in range(self.nVol):
-            cell3DIndex = self.Vessels.mesh.FlatIndexTo3D(cellFlatIndex)
-            if self.labels[cell3DIndex] == 0: # If in tissue, consumption rate non-nill
-                # Here assume cubic control volumes (i.e., dx=dy=dz)
-                M[cellFlatIndex, cellFlatIndex] = -2*D*(dx*dy/dz + dy*dz/dx + dx*dz/dy) - kt*self.v
-                for m in [-1, 1]:
-                    for l in range(3):
-                        neighbourCell = cell3DIndex
-                        neighbourCell[l] += m
-                        neighbourFlatIndex = self.Vessels.mesh.3DToFlatIndex(neighbourCell)
-                        M[cellFlatIndex, neighbourFlatIndex] = D*spacing[j-1]*spacing[j-2]/spacing[j]
-        self.Md = M.tocsr(copy=True)
-        del M
+            cell3DIndex = np.array(self.Vessels.mesh.FlatIndexTo3D(cellFlatIndex))
+            cellLabel = self.labels[cell3DIndex]
+
+            if cellLabel in [0,2]: # Diffusion does not happen in intravascular elements
+                # Add the flux of each face.
+                # Flux is 0 on boundary faces.
+                for m in range(3):
+                    for n in [-1, 1]:
+                        neighbour = cell3DIndex + np.array([m if l==m else 0 for l in range(3)])
+                        if self.Vessels.mesh.IsInsideMesh(neighbour):
+                            neighbhourFlatIndex = self.Vessels.mesh.ToFlatIndexFrom3D(neighbour)
+                            flux = D*spacing[m-1]*spacing[m-2]/spacing[m]
+                            M[cellFlatIndex, cellFlatIndex] -= flux
+                            M[cellFlatIndex, neighbourFlatIndex] = flux
+
+                # Add oxygen consumption if it is a tissue cell
+                if cellLabel == 0:
+                    M[cellFlatIndex, cellFlatIndex] -= kt*v
+
+        self.Md = M.tocsr()
         return
     
-    def MakeConvection(self):
-        # TODO
+    def MakeConvection(self, **kwargs):
+        """Assembles the discretization of the convection problem in vessels.
+
+        The convection operator -div(f.c_v) is discretized using an
+        upwind scheme. At vascular node i with links to j and k, the
+        discretization reads:
+        f_{i,j}(c_i-c_j)/l_{i,j} + f_{i,k}(c_i-c_k)/l_{i,k}.
+        The method solves for blood flow in the vascular network.
+        Boundary conditions and parameters can be specified as keyword arguments.
+
+        Keyword arguments
+        -----------------
+        qProx : float
+            Inlet blood flow.
+        qDist : float
+            Outlet blood flow.
+        pProx : float
+            Inlet blood pressure.
+        pDist : float
+            Outlet blood pressure.        
+        """
+        self.Vessels.SetLinearSystem(kwargs)
+        self.Vessels.SolveFlow()
+        M = sp.dok_array((self.nPoints, self.nPoints), dtype=np.float32)
+        for node in self.Vessels.G.nodes():
+            successors = self.Vessels.G.successors(node)
+            
+            if not self.Vessels.G.pred[node]: 
+                # It is a inlet node
+                print(f"Inlet node equation is at row {node}")
+                M[node, node] = 1
+            else:
+                for otherNode in successors:
+                    flow = self.Vessels.G[node][otherNode]['flow']
+                    length = self.Vessels.G[node][otherNode]['length']
+                    M[node, othernode] = -flow/length
+                    M[node, node] += flow/length
+
+        self.Mc = M.tocsr()
         return
+
+    def MakeRHS(self, cv : float):
+        """Assembles the RHS.
+
+        At the tissue boundary, no-flux conditions are applied. For
+        the vascular O2 convection problem, concentration at inlet is
+        needed.
+
+        Parameters
+        ----------
+        cv : float
+             Inlet concentration of blood oxygen.
+        """
+        cvBar = sp.dok_array((self.nPoints,), dtype=np.float32)
+        for node in self.Vessels.G.nodes():
+            if not self.Vessels.G.pred[node]:
+                # No parent found
+                cvBar[node] = cv
+
+        ctBar = sp.dok_array((self.nVol,), dtype=np.float32)
+        for 
+                
+        
     
