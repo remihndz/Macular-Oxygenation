Fixed the memory issue. We had too many voxels per vascular node causing the overflow when creating the matrix. Now we prune the coupling graph first before creating the matrix. Unfortunately, that makes for less smooth oxygen transfer because some vascular nodes have too few/no neighbors?

Oxygen-Voxel-Method/src/LinearSystems.py
@@ -6,6 +6,8 @@ from graph_tool.generation import lattice
 from graph_tool.spectral import incidence
 from graph_tool.util import find_edge
 from itertools import chain 
+import psutil
+
 
 def ConvectionMatrices(CV):
     C1 = incidence(CV._vessels).T
@@ -18,6 +20,7 @@ def ConvectionMatrices(CV):
     L = sp.dia_matrix((1.0/l.a, 0), shape=2*[CV._vessels.num_edges()])
     Q = sp.dia_matrix((q.a * (l.a/2), 0), shape=2*[CV._vessels.num_edges()])
     Min  = Din @ C1.T @ Q @ (PositivePart(-C1))
+    Min = Din
     MOut = Dout @ C1.T @ Q @ PositivePart(C1)
     M = C1.T@(PositivePart(L@Q)@PositivePart(C1) - PositivePart(-L@Q)@PositivePart(-C1))
     # M -= MOut
@@ -67,40 +70,66 @@ def MakeCouplingMatrices(CV:ControlVolumes, h_v:float):
     ### Returns
     - scipy sparse matrices C2v, C2t and S.
     """
-    couplingGraph = CV.LabelMesh(h_v)
+    C2v, C2t, N = CV.LabelMesh(h_v)
     C1 = incidence(CV._vessels)
     S = np.pi*CV._vessels.ep['radius'].a*(CV._vessels.ep['length'].a/2.0) 
     S = abs(C1)@S # Scale the exchange rates by the surface area of the control volume
     
-    # The list of endtohelial edges between voxels and control volumes
-    endo = find_edge(couplingGraph, couplingGraph.ep['eType'],1)
-    nc, nt, nv = len(endo), CV.grid.size, CV._vessels.num_vertices() 
-    
-    ## Initialize the matrices
-    C2v = sp.dok_matrix((nc,nv))
-    C2t = sp.dok_matrix((nc,nt))
-    N   = np.zeros(nv, dtype=int) # Number of connections to each control volumes. 
-                        # Should be the same as C2v.sum(1)
-    for i, e in enumerate(endo):
-        u,v = int(e.source()), int(e.target()) # u: a control volume, v: a voxel
-        C2v[i, u] = 1 
-        C2t[i, v] = 1 
-        N[u]+=1
-    C2v, C2t = C2v.tocsr(), C2t.tocsr()
-    S = np.multiply(1/N, S)
+    nc = C2v.shape[0]
+    S = np.multiply(1/np.where(N>0, N, np.inf), S)
     S = sp.dia_matrix(([si for si,Ni in zip(S,N) for _ in range(Ni)], 0),
                       shape=(nc,nc))
     return C2v, C2t, S
 
 def MakeDiffusionReaction(grid:Grid, Gamma_t:float, k_t:float):
-    lat = lattice(grid.shape)
-    lat.set_directed(True)
-    C3 = incidence(lat)
-    D3 = sp.dia_matrix(([d!=6 for d in lat.get_total_degrees(lat.get_vertices())], 0)
-                       , shape=(grid.size, grid.size))
-    I3 = sp.eye(grid.size)
+    # lat = lattice(grid.shape)
+    # lat.set_directed(True)
+    # C3 = incidence(lat, operator=True)
+    # print("C3, done. CPU:", psutil.virtual_memory())
+    # print("Diffusion matrix, done. CPU:", psutil.virtual_memory())
+    # D3 = spl.aslinearoperator(
+    #     sp.dia_matrix(([d!=6 for d in lat.get_total_degrees(lat.get_vertices())], 0)
+    #                    , shape=(grid.size, grid.size))
+    #     ) 
+    # I3 = spl.aslinearoperator(sp.eye(grid.size))
+    # del lat    
+    # B = (I3-D3)@(-Gamma_t * (grid.h[0]) * C3@C3.T + k_t * np.prod(grid.h) * I3) + D3
+    ijk = np.arange(grid.size).reshape(grid.shape)
+    boundaryIndices = np.flatnonzero(ijk)
+    del ijk
+
+    def _D3(v):
+        y = np.zeros_like(v)
+        y[boundaryIndices] = v[boundaryIndices]
+        return y
+    def _I3minusD3(v):
+        v[boundaryIndices] = 0
+        return v
+    def _L(x):
+        grid_shape = tuple(grid.shape)
+        N = len(grid_shape)
+        X = x.reshape(grid_shape + (-1,))
+        Y = -2 * N * X
+        for i in range(N):
+            Y += np.roll(X, 1, axis=i)
+            Y += np.roll(X, -1, axis=i)
+            Y[
+                (slice(None),) * i + (0,) + (slice(None),) * (N-i-1)
+            ] += np.roll(X, 0, axis=i)[
+                (slice(None),) * i + (0,) + (slice(None),) * (N-i-1)
+                ]
+            Y[
+                (slice(None),) * i + (-1,) + (slice(None),) * (N-i-1)
+            ] += np.roll(X, 0, axis=i)[
+                (slice(None),) * i + (-1,) + (slice(None),) * (N-i-1)
+                ]
+            return Y.reshape(-1, X.shape[-1])
     
-    B = (I3-D3)@(-Gamma_t * (grid.h[0]) * C3@C3.T + k_t * np.prod(grid.h) * I3) + D3
+    L  = spl.LinearOperator(shape=(grid.size, grid.size), matvec=_L)
+    I3 = spl.LinearOperator(shape=L.shape, matvec=lambda v: v, matmat=lambda V:V, rmatvec=lambda v:v)
+    D3 = spl.LinearOperator(shape=L.shape, matvec=_D3)
+    I3minusD3 = spl.LinearOperator(shape=L.shape, matvec=_I3minusD3)
+    B = I3minusD3@(-Gamma_t * (grid.h[0]) * L + k_t * np.prod(grid.h) * I3) + D3 
     return B, D3
 
 def MakeAll(
@@ -109,22 +138,34 @@ def MakeAll(
         cvIn:float, # Oxygen concentration at vascular inlets
         ctBC:float, # Oxygen concentration on cube boundaries
         Gamma_v:float, h_v:float, # Vessel wall permeability and thickness 
-        Gamma_t:float, k_t:float, # Tissue diffusion and consumption     
+        Gamma_t:float, k_t:float, # Tissue diffusion and consumption rates     
         ): 
     M, Min, Din = ConvectionMatrices(CV)
-    nc, nt, nv = C2v.shape[0], CV.grid.size, CV._vessels.num_vertices() 
+    nc, nt, nv = C2v.shape[0], CV.grid.size, CV._vessels.num_vertices()
 
+    # M, Min = spl.aslinearoperator(M), spl.aslinearoperator(Min)
+    # C2v, C2t, S = spl.aslinearoperator(C2v), spl.aslinearoperator(C2t), spl.aslinearoperator(S)
+    
     # Convection + free O2 in plasma at the interface with tissue
     # sp.eye(nv)-Din cancels exchange for the inlet (BC)
     A = M + Min # Convection 
-    A += Gamma_v/h_v * C2v.T@S@C2v @ (sp.eye(nv)-Din) # Sinks
+    A += Gamma_v/h_v * (sp.eye(nv)-Din) @ C2v.T@S@C2v  # Sinks
+    print("A, done. CPU:", psutil.virtual_memory())
+    del M
+    
+    C2v, C2t, S = spl.aslinearoperator(C2v), spl.aslinearoperator(C2t), spl.aslinearoperator(S)
+    
     E = -Gamma_v/h_v * (sp.eye(nv)-Din) @ C2v.T@S@C2t # Tissue oxygen at interface with vessel 
+    print("E, done. CPU:", psutil.virtual_memory())
     
     B, D3 = MakeDiffusionReaction(CV.grid, Gamma_t, k_t)
+    print("Got diffusion. CPU:", psutil.virtual_memory())
     B -= Gamma_v/h_v * C2t.T@S@C2t # Oxygen sources
+    print("B, done. CPU:", psutil.virtual_memory())
     G = Gamma_v/h_v * C2t.T@S@C2v  # Plasma free oxygen concentration
-
-    cvBar = Din@Min@(cvIn*np.ones(nv))
+    print("G, done. CPU,", psutil.virtual_memory())
+    del C2t, C2v, S
+    cvBar = Min@(cvIn*np.ones(nv))
     ctBar = D3 @ (np.ones(nt) * ctBC)
 
     return [[A,E], [G,B]], [cvBar, ctBar]

Oxygen-Voxel-Method/src/utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 # import numba
+import warnings
 
 import scipy.sparse as sp
 import numpy as np
@@ -370,45 +371,68 @@ class ControlVolumes(object):
         # endo = self.VAG.new_vertex_property("python::object", val=set())
         # vasc = self.VAG.new_vertex_property("python::object", val=set())
 
-        G = self.VAG.copy()
+        G = self.VAG.copy() # Coupling graph
+        vascularVertices = np.array(list(G.iter_vertices()))
+        nVAG = max(max(vascularVertices), len(vascularVertices))
+
         G.clear_edges()
-        G.shrink_to_fit()
-        Voxels = np.fromiter(G.add_vertex(self.grid.size), dtype=int)
+        for name in list(G.vp): # Remove unnecessary properties that were copied
+            del G.vp[name]
+            try: # Check if they are still in the original graph
+                _ = self.VAG.vp[name] 
+            except KeyError:
+                warnings.warn("Some vertex property maps have been deleted from the vascular graph.")
+        G.shrink_to_fit() # reduce memory usage to what's needed only
+        
+        # Voxels = np.fromiter(G.add_vertex(self.grid.size), dtype=int) ## add_vertex scales in O(n) which makes it very slow for this. We don't need all the voxels in the graph.
         eType = G.new_edge_property("bool") # 0 for vascular, 1 for endothelial connections
         G.ep['eType'] = eType
 
+        def Prune():
+            Voxels = G.get_vertices()
+            Voxels = np.setxor1d(Voxels, vascularVertices) # Should get rid of the vascular vertices
+            Voxels = Voxels[G.get_in_degrees(Voxels)>0]        
+            G.set_fast_edge_removal(fast=True) # This requires a data structure of size O(E) to be kept
+            edgesToKeep = []
+            for v in tqdm(Voxels, desc='Pruning the coupling graph'):
+                edges = G.get_all_edges(v, eprops=[eType])
+                endothelialEdges = edges[:,-1]==1
+                endothelialEdges = edges[endothelialEdges]
+                if len(endothelialEdges): # If any endothelial edge
+                    edge = endothelialEdges[0]
+                    edgesToKeep.append(edge) # Edge to keep
+                    G.clear_vertex(v)
+            G.add_edge_list(edgesToKeep, eprops=[G.ep['eType']])
+            del Voxels
+            G.shrink_to_fit() # reduce memory usage to what's needed only
+
+        
         for e in tqdm(self.VAG.iter_edges(), 
                         total=self.VAG.num_edges(), 
-                        desc="Labelling...", unit="edge"): # Loop through the vessels in the FOV.
+                        desc="Labelling...", unit="edge"): # Loop through the vessels in the FOV.            
             for u, voxelsSet, t in zip((e[0],e[0], e[1], e[1]), LabelVessel(e,w), (1,0,1,0)):
-                G.add_edge_list(((u, v, t) for v in voxelsSet), eprops=[eType])
-            # print(f"{G.num_edges()=}, {G.num_vertices()=}")
+                G.add_edge_list(((u, nVAG+v, t) for v in voxelsSet), eprops=[eType])
         del voxelsSet
 
-        edgesToKeep = []
-        Voxels = Voxels[G.get_total_degrees(Voxels)>0]
-        for v in tqdm(Voxels):
-            edges = G.get_all_edges(v, eprops=[eType])
-            if (edges[:,-1]==1).any():
-                edge = edges[edges[:,-1]==1][0]
-            else:
-                edge = edges[0]
-            edgesToKeep.append(edge)
-        # Make it an internal property
-        self.VAG.vp['endothelium'] = self.VAG.new_vertex_property("python::object", val=[])
-        self.VAG.vp['vascular']    = self.VAG.new_vertex_property("python::object", val=[])
+        # Prune the coupling graph to one endothelial connection per voxel
+        Prune()
 
-        vasc = find_edge(G, eType, 0)
-        endo = find_edge(G, eType, 1)
-        vertex_index = G.vertex_index
-        for cv, voxel in tqdm(vasc, desc="Adding vascular voxels to graph"):
-            self.VAG.vp['vascular'][cv].append(vertex_index[voxel])
-        for cv, voxel in tqdm(endo, desc="Adding endothelium voxels to graph"):
-            self.VAG.vp['endothelium'][cv].append(vertex_index[voxel])
+        ## Coupling matrices
+        endo = find_edge(G, G.ep['eType'],1)
+        nc, nt, nv = len(endo), self.grid.size, self._vessels.num_vertices() 
 
-        # self.VAG.vp['endothelium'] = endo
-        # self.VAG.vp['vascular']    = vasc
-        return G
+        C2v = sp.dok_matrix((nc,nv))
+        C2t = sp.dok_matrix((nc,nt))
+        N   = np.zeros(nv, dtype=int) # Number of connections to each control volumes. 
+                            # Should be the same as C2v.sum(1)
+        for i, e in enumerate(endo):
+            u,v = int(e.source()), int(e.target())-nVAG # u: a control volume, v: a voxel
+            C2v[i, u] = 1 
+            C2t[i, v] = 1 
+            N[u]+=1
+        C2v, C2t = C2v.tocsr(), C2t.tocsr()
+        del endo, G
+        return C2v.tocsr(), C2t.tocsr(), N
 
     def bar(self): # To profile the time for labelling.
         profile.runctx('self.LabelMesh()', globals(), locals())
@@ -551,8 +575,7 @@ class gmres_counter(object):
     def __call__(self, rk=None):
         self.niter += 1
         if self.niter%self._disp==0:
-            if isinstance(rk, np.ndarray):
-                rk = rk.get()
+            rk = np.mean(rk)
             self.rks.append(rk)
             print('iter %3i\trk = %s' % (self.niter, str(rk)))
 
@@ -564,7 +587,7 @@ class gmres_counter(object):
         plt.plot(self._disp * (1+np.arange(len(self.rks))), self.rks)
         plt.xlabel("Number of iterations")
         plt.ylabel("Residual (relative?)")
-        plt.title(f"Solving time: {t}min")
+        plt.title(f"Solving time: {t:.4f}min")
         plt.show()
 
 